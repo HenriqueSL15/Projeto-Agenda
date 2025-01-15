c7e42bef9eb6f1dc8cd7dcf3a68c5facd6b2d74b
Henrique Siqueira Lima <henrynyyls@gmail.com>
2025-01-15 18:52:01 -0300

    New css added and new content "section" for all the front-end

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Henry\OneDrive\Área de Trabalho\Projetos\Full-Stack\Projeto-Agenda\venv"
+set "VIRTUAL_ENV=C:\Users\Henry\OneDrive\Documentos\Projetos\Full-Stack\Projeto-Agenda\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
