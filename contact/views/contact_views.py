5a231206bc00c92f7d98ad501ef30272443c3c78
Henrique Siqueira Lima <henrynyyls@gmail.com>
2025-01-19 20:01:00 -0300

    Added a header

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
+from django.db.models import Q
 from django.http import Http404
 
 # Create your views here.
@@ -8,6 +9,33 @@ def index(request):
 
     context = {
         'contacts': contact,
+        'site_title': 'Contatos - '
+    }    
+
+    return render(
+        request,
+        'contact/index.html',
+        context,
+        )
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+    contact = Contact.objects.filter(show=True) \
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+
+        )\
+        .order_by('-id')
+
+    if search_value == "":
+        return redirect('contact:index')
+
+    context = {
+        'contacts': contact,
+        'site_title': 'Search - '
     }    
 
     return render(
@@ -20,11 +48,14 @@ def contact(request, contact_id):
     # single_contact = Contact.objects.filter(pk=contact_id).first()
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
 
+    single_contact_name = f'{single_contact.first_name} {single_contact.last_name} - '
+
     if single_contact is None:
         raise Http404()
 
     context = {
         'contact': single_contact,
+        'site_title': single_contact_name
     }    
 
     return render(
