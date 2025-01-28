e4a3424ff5efddb74896ac2472dc25e45d01d7dd
Henrique Siqueira Lima <henrynyyls@gmail.com>
2025-01-28 18:57:47 -0300

    Added  a create url to create users (the creating system doesn't work yet, only the url system is working at this moment)

---
+++ contact/views/contact_forms.py
@@ -0,0 +1,17 @@
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
+
+from contact.models import Contact
+
+def create(request):
+    print(request.POST.get('first_name'))
+
+    context ={
+
+    }
+    return render(
+        request,
+        'contact/create.html',
+        context
+    )
