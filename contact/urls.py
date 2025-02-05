a84b9f3cc600afc1aa23c79407da0b4afae7e0e9
Henrique Siqueira Lima <henrynyyls@gmail.com>
2025-02-05 18:11:28 -0300

    Added a confirmation for when deleting a user information

--- contact/urls.py
+++ contact/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('contact/<int:contact_id>/detail/', views.contact, name="contact"),
     path('contact/create/', views.create, name="create"),
     path('/contact/<int:contact_id>/update/', views.update, name="update"),
-    # path('/contact/<int:contact_id>/delete/', views.contact, name="contact"),
+    path('/contact/<int:contact_id>/delete/', views.delete, name="delete"),
 
     # # users (CRUD)
     # path('/users/<int:contact_id>/detail/', views.contact, name="contact"),
